9d874cf265c3a58197c6e3c4bd4bf6830c3fcbab
evanson <igiri.evanson@gmail.com>
2019-07-14 17:15:58 +0100

    configure the code to pass the adc test grader

--- server/middleware/inputValidator.js
+++ server/middleware/inputValidator.js
@@ -8,7 +8,7 @@ export const checkSignup = Joi.object().keys({
   email:Joi.string().email(),//.required(),
   first_name:Joi.string(),//.min(3).required(),
   last_name: Joi.string(),//.min(3).required(),
-  password: Joi.string().alphanum(),//.regex(/^[a-zA-Z0-9]{6,16}$/).min(6).required(),
+  password: Joi.string().regex(/^[a-zA-Z0-9]{6,16}$/).min(6).required(),
   phone_number: Joi.number(),//.min(1).required(),
   address: Joi.string()//.min(3).regex(/^[\w\-\s]+$/).required()
 });
