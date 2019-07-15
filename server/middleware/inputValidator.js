c85444e850135a2714d5272a410830b893873b9c
evanson <igiri.evanson@gmail.com>
2019-07-15 22:41:47 +0100

    refactoring  my validtions

--- server/middleware/inputValidator.js
+++ server/middleware/inputValidator.js
@@ -8,15 +8,15 @@ export const checkSignup = Joi.object().keys({
   email:Joi.string().email().required(),
   first_name:Joi.string().min(3).required(),
   last_name: Joi.string().min(3).required(),
-  password: Joi.string().regex(/^[a-zA-Z0-9]{6,16}$/).min(6).required(),
+  password: Joi.string()/*.regex(/^[a-zA-Z0-9]{6,16}$/)*/.min(6).required(),
   phone_number: Joi.number().min(1).required(),
-  address: Joi.string().min(3).regex(/^[\w\-\s]+$/).required()
-});
+  address: Joi.string().min(3)/*.regex(/^[\w\-\s]+$/)*/.required()
+}).unknown(true);
 
 
 export const checkSignin = Joi.object().keys({
   email: Joi.string().email().required(),
-  password: Joi.string().regex(/^[a-zA-Z0-9]{6,16}$/).min(6).required()
+  password: Joi.string()/*.regex(/^[a-zA-Z0-9]{6,16}$/)*/.min(6).required()
 });
 
       
@@ -27,9 +27,9 @@ export const checkSignin = Joi.object().keys({
   state: Joi.string().required(),
   city: Joi.string().required(),
   address: Joi.string().min(3).required(),
-  type: Joi.string().regex(/^[\w\-\s]+$/).required(),   
+  type: Joi.string()/*.regex(/^[\w\-\s]+$/)*/.required(),   
   image_url: Joi.string()
-});
+}).unknown(true);
 
 
  export const checkFlag = Joi.object().keys({
