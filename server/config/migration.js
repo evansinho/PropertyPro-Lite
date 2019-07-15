08ede8153b801d3a1fb0c273e5ea15bcc3f0145b
evansinho <43210534+evansinho@users.noreply.github.com>
2019-07-15 17:07:41 +0100

    Update migration.js

--- server/config/migration.js
+++ server/config/migration.js
@@ -10,7 +10,7 @@ const migration = `
 	  id serial UNIQUE PRIMARY KEY NOT NULL,
 	  email VARCHAR(128) UNIQUE NOT NULL,
 	  first_name VARCHAR(128) NOT NULL,
-	  last_name VARCHAR(128) NOT NULL,
+	  last_name VARCHAR(128),
 	  password varchar(100) NOT NULL,
 	  phone_number varchar(100) NOT NULL,
 	  address VARCHAR(128) NOT NULL
